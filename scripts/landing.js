84f138b096aa92df99b11bc07cb298374eb368c2
clarefahey <fahey.clare@gmail.com>
2017-08-21 21:00:13 -0400

    fixing changes

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 var pointsArray = document.getElementsByClassName('point');
 
 var revealPoint = function(point) {
@@ -25,60 +22,3 @@ window.onload = function() {
          }
     });
   }
-=======
-var animatePoints = function() {
-=======
-var pointsArray = document.getElementsByClassName('point');
->>>>>>> checkpoint-9-new
-
-var animatePoints = function(points) {
-  var revealPoint = function(index) {
-      points[index].style.opacity = 1;
-      points[index].style.transform = "scaleX(1) translateY(0)";
-      points[index].style.msTransform = "scaleX(1) translateY(0)";
-      points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
-     for (var i = 0; i < points.length; i++) {
-       revealPoint(i);
-     }
- };
-
- window.onload = function() {
-
-   if (window.innerHeight > 950) {
-         animatePoints(pointsArray);
-     }
-
-   var sellingPoints = document.getElementsByClassName('selling-points')[0];
-   var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-
-<<<<<<< HEAD
- };
->>>>>>> checkpoint-8
-=======
-var animatePoints = function() {
-    var points = document.getElementsByClassName('point');
-    var revealPoint = function(index) {
-      points[index].style.opacity = 1;
-      points[index].style.transform = "scaleX(1) translateY(0)";
-      points[index].style.msTransform = "scaleX(1) translateY(0)";
-      points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-    }
-     for (var i = 0; i < points.length; i++) {
-       revealPoint(i);
-     }
- };
->>>>>>> checkpoint-8-assignment
-=======
-   window.addEventListener("scroll", function(event) {
-     if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-             animatePoints(pointsArray);
-         }
-   });
-  }
-<<<<<<< HEAD
->>>>>>> checkpoint-9-new
-=======
-
- 
->>>>>>> checkpoint-9-new
