859aa0132173856d11e4cd0fe8adb71fe13e91a6
clarefahey <fahey.clare@gmail.com>
2017-08-24 21:09:52 -0400

    final final changes to checkpoint 9

--- scripts/landing.js
+++ scripts/landing.js
@@ -13,19 +13,16 @@ var animatePoints = function(points) {
  };
 
  window.onload = function() {
-
    if (window.innerHeight > 950) {
-         animatePoints(pointsArray);
-     }
-
+    animatePoints(pointsArray);
+  }
+  
    var sellingPoints = document.getElementsByClassName('selling-points')[0];
    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
 
-   window.addEventListener("scroll", function(event) {
+   window.addEventListener('scroll', function(event) {
      if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
              animatePoints(pointsArray);
          }
-   });
-  }
-
- 
+    });
+ }
