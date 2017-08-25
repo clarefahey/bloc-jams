2b3c3f4b23f62bb3b3166360ab75639dc7c252bf
clarefahey <fahey.clare@gmail.com>
2017-08-24 21:20:21 -0400

    final checkpoint 9

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,29 +1,12 @@
 var pointsArray = document.getElementsByClassName('point');
 
-var revealPoint = function(point) {
-          point.style.opacity = 1;
-          point.style.transform = "scaleX(1) translateY(0)";
-          point.style.msTransform = "scaleX(1) translateY(0)";
-          point.style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
-<<<<<<< HEAD
-
 var animatePoints = function(points) {
-  forEach(points, revealPoint);
-};
-
-window.onload = function() {
-    if (window.innerHeight > 950) {
-    animatePoints(pointsArray);
-    }
-
-    window.addEventListener("scroll", function(event) {
-      if (pointsArray[0] .getBoundingClientRect().top <= 500) {
-             animatePoints(pointsArray);
-         }
-    });
-  }
-=======
+  var revealPoint = function(index) {
+      points[index].style.opacity = 1;
+      points[index].style.transform = "scaleX(1) translateY(0)";
+      points[index].style.msTransform = "scaleX(1) translateY(0)";
+      points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+    };
      for (var i = 0; i < points.length; i++) {
        revealPoint(i);
      }
@@ -33,7 +16,7 @@ window.onload = function() {
    if (window.innerHeight > 950) {
     animatePoints(pointsArray);
   }
-  
+
    var sellingPoints = document.getElementsByClassName('selling-points')[0];
    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
 
@@ -43,4 +26,3 @@ window.onload = function() {
          }
     });
  }
->>>>>>> checkpoint-9-new
